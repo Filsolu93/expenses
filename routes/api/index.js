6e0494726225ca28f73b282e6440d572a198d8ea
Tarik P <tarikdeshon@gmail.com>
2019-08-17 13:53:07 -0700

    creating branch

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const exampleRoutes = require("./example");
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use("/example", exampleRoutes);
 
 module.exports = router;
