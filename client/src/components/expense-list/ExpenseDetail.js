e345878ec810e369e50052ec8db675034239961e
Filmon habtu <filsolu93@gmail.com>
2019-08-20 20:22:24 -0700

    frontend

--- client/src/components/expense-list/ExpenseDetail.js
+++ client/src/components/expense-list/ExpenseDetail.js
@@ -2,24 +2,30 @@ import React, {
     Component
 } from "react";
 import ExpenseIcon from "./ExpenseIcon";
+
 export default class ExpenseDetail extends Component {
     formatDate(date) {
         const dateParts = date.split("-");
         return `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
     }
-render() {return ( liclassName = ExpenseIconcategory = {this:props.expense.category}>
-            <span className = "mdc-list-item__text" > {
-                this.props.expense.category
-            } <span className = "mdc-list-item__text__secondary" > {
-                this.formatDate(this.props.expense.date)} 
-                { this.props.expense.description ?
-                    ` ${this.props.expense.description.replace(/^(.{14}).+/, "$1…")}` :
-                    ""
-            } </span> </span> <span className = "mdc-list-item__end-detail" >
-            $ {
-                this.props.expense.amount
-            } </span>
-            <ul/>
-);
-                   }
+render() {
+    return ( 
+    <li className = "mdc-list-item"
+        onClick = {() => this.props.onSelect(this.props.expense)} ><ExpenseIcon category = {
+            this.props.expense.category
+        }
+        /> <span className = "mdc-list-item__text" > {
+            this.props.expense.category
+        } <span className = "mdc-list-item__text__secondary" > {
+            this.formatDate(this.props.expense.date)
+        } {
+            this.props.expense.description ?
+                ` ${this.props.expense.description.replace(/^(.{14}).+/, "$1…")}` :
+                ""
+        } </span> </span> < span className = "mdc-list-item__end-detail" >
+        $ {
+            this.props.expense.amount
+        } </span> </li>
+    );
+}
 }
